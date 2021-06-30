63fff4f1c8fc323091db6630a53d1d38c5c2c06c
Maximiliano <maxirpc2607@gmail.com>
2021-06-30 08:15:04 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,24 @@ console.log("Hola Mundo desde el DOM");
 
 // validar formulario newsletter
 
-const formNewsletter = document.getElementById("newsletter");
-
+var formNewsletter = document.getElementById("newsletter");
+console.log(formNewsletter);
+formNewsletter.addEventListener("submit", (e) => {
+	e.preventDefault();
+	console.log("form");
+});
+/*
 formNewsletter.addEventListener("submit", (e) => {
 	e.preventDefault();
 
 	var email = document.getElementById("email");
 	if (email.value == "") {
 		email.classList.add("is-invalid");
+		console.log("if");
 	} else {
 		email.classList.remove("is-invalid");
+		console.log("else");
 	}
+	console.log("form");
 });
+*/
